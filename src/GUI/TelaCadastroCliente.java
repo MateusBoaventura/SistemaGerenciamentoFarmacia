2670db692f960a7f24c19e264b07fa3de07a8e76
victo <victo@DESKTOP-A4964DS>
2023-06-06 22:28:52 -0300

    Finalmente paz

--- src/GUI/TelaCadastroCliente.java
+++ src/GUI/TelaCadastroCliente.java
@@ -1,75 +1,112 @@
 package GUI;
 
-import javax.swing.JPanel;
-import javax.swing.JTextPane;
-import java.awt.Font;
-import java.awt.Color;
-import java.awt.Component;
+import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import com.excript.Farmacia.Cliente;
+
+import excessoes.JaCadastrado;
+import main.Main;
+
+import javax.swing.JTextPane;
+import java.awt.Color;
+import java.awt.Font;
 import javax.swing.JTextField;
 import java.awt.SystemColor;
 import javax.swing.JButton;
-import javax.swing.JOptionPane;
-
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import com.excript.Farmacia.*;
 
-import excessoes.JaCadastrado;
+public class TelaCadastroCliente extends JFrame {
 
-public class TelaCadastroCliente extends JPanel {
+	private JPanel contentPane;
 	private JTextField txtNome;
 	private JTextField txtEmail;
-	private JTextField txtTelefone;
 	private JTextField txtCPF;
-	protected Component contentPane;
+	private JTextField txtTelefone;
 
 	/**
-	 * Create the panel.
+	 * Launch the application.
 	 */
-	public TelaCadastroCliente(Farmacia farmacia) {
-		setLayout(null);
+	public static void main(String[] args) {
+		
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaCadastroCliente(String funcionarioLogado) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
 		JTextPane txtpnNome = new JTextPane();
-		txtpnNome.setBackground(new Color(240, 240, 240));
 		txtpnNome.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		txtpnNome.setText("Nome:");
-		txtpnNome.setBounds(10, 25, 53, 20);
-		add(txtpnNome);
+		txtpnNome.setBackground(new Color(240, 240, 240));
+		txtpnNome.setText("Nome: ");
+		txtpnNome.setBounds(10, 11, 41, 20);
+		contentPane.add(txtpnNome);
 		
 		txtNome = new JTextField();
-		txtNome.setBounds(73, 25, 367, 20);
-		add(txtNome);
+		txtNome.setBounds(76, 11, 348, 20);
+		contentPane.add(txtNome);
 		txtNome.setColumns(10);
 		
 		JTextPane txtpnEmail = new JTextPane();
-		txtpnEmail.setText("Email:");
+		txtpnEmail.setText("Email: ");
 		txtpnEmail.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		txtpnEmail.setBackground(SystemColor.menu);
-		txtpnEmail.setBounds(10, 56, 53, 20);
-		add(txtpnEmail);
+		txtpnEmail.setBounds(10, 42, 41, 20);
+		contentPane.add(txtpnEmail);
 		
 		txtEmail = new JTextField();
 		txtEmail.setColumns(10);
-		txtEmail.setBounds(73, 56, 367, 20);
-		add(txtEmail);
+		txtEmail.setBounds(76, 42, 348, 20);
+		contentPane.add(txtEmail);
 		
-		JTextPane txtpnTelefone = new JTextPane();
-		txtpnTelefone.setText("Telefone:");
-		txtpnTelefone.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		txtpnTelefone.setBackground(SystemColor.menu);
-		txtpnTelefone.setBounds(10, 87, 62, 20);
-		add(txtpnTelefone);
+		JTextPane txtpnNome_1_1 = new JTextPane();
+		txtpnNome_1_1.setText("CPF: ");
+		txtpnNome_1_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		txtpnNome_1_1.setBackground(SystemColor.menu);
+		txtpnNome_1_1.setBounds(10, 73, 41, 20);
+		contentPane.add(txtpnNome_1_1);
+		
+		txtCPF = new JTextField();
+		txtCPF.setColumns(10);
+		txtCPF.setBounds(76, 73, 348, 20);
+		contentPane.add(txtCPF);
+		
+		JTextPane txtpnNome_1_1_1 = new JTextPane();
+		txtpnNome_1_1_1.setText("Telefone: ");
+		txtpnNome_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		txtpnNome_1_1_1.setBackground(SystemColor.menu);
+		txtpnNome_1_1_1.setBounds(10, 104, 66, 20);
+		contentPane.add(txtpnNome_1_1_1);
 		
 		txtTelefone = new JTextField();
 		txtTelefone.setColumns(10);
-		txtTelefone.setBounds(73, 87, 367, 20);
-		add(txtTelefone);
+		txtTelefone.setBounds(76, 104, 348, 20);
+		contentPane.add(txtTelefone);
 		
 		JButton btnNewButton = new JButton("Voltar");
-		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnNewButton.setBounds(10, 266, 89, 23);
-		add(btnNewButton);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				TelaFuncionario telaFunc = new TelaFuncionario(funcionarioLogado);
+				telaFunc.setVisible(true);
+				dispose();
+			}
+		});
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnNewButton.setBounds(10, 227, 89, 23);
+		contentPane.add(btnNewButton);
 		
 		JButton btnCadastrar = new JButton("Cadastrar");
 		btnCadastrar.addActionListener(new ActionListener() {
@@ -82,27 +119,17 @@ public class TelaCadastroCliente extends JPanel {
 				Cliente cliente = new Cliente(nome, email, telefone, cpf, 0);
 				
 				try {
-					farmacia.cadastrarCliente(cliente);
+					Main.FARMACIA.cadastrarCliente(cliente);
+					JOptionPane.showMessageDialog(contentPane,"Cliente cadastrado");
+
 				} catch (JaCadastrado ex) {
 					JOptionPane.showMessageDialog(contentPane,"Erro, cliente já cadastrado");
 				}
 			}
 		});
-		btnCadastrar.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnCadastrar.setBounds(325, 268, 115, 23);
-		add(btnCadastrar);
-		
-		JTextPane txtpnCpf = new JTextPane();
-		txtpnCpf.setText("CPF");
-		txtpnCpf.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		txtpnCpf.setBackground(SystemColor.menu);
-		txtpnCpf.setBounds(10, 118, 62, 20);
-		add(txtpnCpf);
-		
-		txtCPF = new JTextField();
-		txtCPF.setColumns(10);
-		txtCPF.setBounds(73, 118, 367, 20);
-		add(txtCPF);
-		
+		btnCadastrar.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnCadastrar.setBounds(335, 227, 89, 23);
+		contentPane.add(btnCadastrar);
 	}
+
 }
--- src/GUI/TelaCadastroExame.java
+++ src/GUI/TelaCadastroExame.java
@@ -1,155 +1,181 @@
 package GUI;
 
-import javax.swing.JPanel;
-import javax.swing.JTextPane;
-import java.awt.Font;
-import java.awt.Color;
-import java.awt.Component;
-
-import javax.swing.JTextField;
-import java.awt.SystemColor;
-import javax.swing.JButton;
-import javax.swing.JOptionPane;
-
-import java.awt.event.ActionListener;
-import java.time.LocalDateTime;
-import java.awt.event.ActionEvent;
+import java.awt.EventQueue;
+import java.util.Date;
 import com.excript.Farmacia.*;
 
-import excessoes.JaCadastrado;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
-public class TelaCadastroExame extends JPanel {
+import com.excript.Farmacia.Cliente;
+import com.excript.Farmacia.Exame;
+
+import main.Main;
+
+import javax.swing.JTextPane;
+import java.awt.Color;
+import java.awt.SystemColor;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.awt.event.ActionEvent;
+
+public class TelaCadastroExame extends JFrame {
+
+	private JPanel contentPane;
 	private JTextField txtNome;
 	private JTextField txtEmail;
-	private JTextField txtTelefone;
 	private JTextField txtCPF;
-	protected Component contentPane;
+	private JTextField txtTelefone;
 	private JTextField txtTipo;
 	private JTextField txtHorario;
 	private JTextField txtPreco;
 
 	/**
-	 * Create the panel.
+	 * Launch the application.
 	 */
-	public TelaCadastroExame(CadastroDeExames cadastroExame) {
-		setLayout(null);
+	public static void main(String[] args) {
+		
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaCadastroExame(String funcioanarioLogado) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
 		JTextPane txtpnNome = new JTextPane();
 		txtpnNome.setBackground(new Color(240, 240, 240));
-		txtpnNome.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		txtpnNome.setText("Nome:");
-		txtpnNome.setBounds(10, 25, 53, 20);
-		add(txtpnNome);
-		
-		txtNome = new JTextField();
-		txtNome.setBounds(73, 25, 367, 20);
-		add(txtNome);
-		txtNome.setColumns(10);
+		txtpnNome.setText("Nome: ");
+		txtpnNome.setBounds(10, 11, 41, 20);
+		contentPane.add(txtpnNome);
 		
 		JTextPane txtpnEmail = new JTextPane();
-		txtpnEmail.setText("Email:");
-		txtpnEmail.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		txtpnEmail.setText("Email: ");
 		txtpnEmail.setBackground(SystemColor.menu);
-		txtpnEmail.setBounds(10, 56, 53, 20);
-		add(txtpnEmail);
+		txtpnEmail.setBounds(10, 42, 41, 20);
+		contentPane.add(txtpnEmail);
+		
+		JTextPane txtpnCpf = new JTextPane();
+		txtpnCpf.setText("CPF: ");
+		txtpnCpf.setBackground(SystemColor.menu);
+		txtpnCpf.setBounds(10, 73, 41, 20);
+		contentPane.add(txtpnCpf);
+		
+		JTextPane txtpnTelefone = new JTextPane();
+		txtpnTelefone.setText("Telefone: ");
+		txtpnTelefone.setBackground(SystemColor.menu);
+		txtpnTelefone.setBounds(10, 104, 56, 20);
+		contentPane.add(txtpnTelefone);
+		
+		JTextPane txtpnTipo = new JTextPane();
+		txtpnTipo.setText("Tipo: ");
+		txtpnTipo.setBackground(SystemColor.menu);
+		txtpnTipo.setBounds(10, 135, 41, 20);
+		contentPane.add(txtpnTipo);
+		
+		JTextPane txtpnHorrio = new JTextPane();
+		txtpnHorrio.setText("Horário: ");
+		txtpnHorrio.setBackground(SystemColor.menu);
+		txtpnHorrio.setBounds(10, 166, 41, 20);
+		contentPane.add(txtpnHorrio);
+		
+		JTextPane txtpnPreo = new JTextPane();
+		txtpnPreo.setText("Preço: ");
+		txtpnPreo.setBackground(SystemColor.menu);
+		txtpnPreo.setBounds(10, 197, 41, 20);
+		contentPane.add(txtpnPreo);
+		
+		txtNome = new JTextField();
+		txtNome.setBounds(72, 11, 352, 20);
+		contentPane.add(txtNome);
+		txtNome.setColumns(10);
 		
 		txtEmail = new JTextField();
 		txtEmail.setColumns(10);
-		txtEmail.setBounds(73, 56, 367, 20);
-		add(txtEmail);
+		txtEmail.setBounds(72, 42, 352, 20);
+		contentPane.add(txtEmail);
 		
-		JTextPane txtpnTelefone = new JTextPane();
-		txtpnTelefone.setText("Telefone:");
-		txtpnTelefone.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		txtpnTelefone.setBackground(SystemColor.menu);
-		txtpnTelefone.setBounds(10, 87, 62, 20);
-		add(txtpnTelefone);
+		txtCPF = new JTextField();
+		txtCPF.setColumns(10);
+		txtCPF.setBounds(72, 73, 352, 20);
+		contentPane.add(txtCPF);
 		
 		txtTelefone = new JTextField();
 		txtTelefone.setColumns(10);
-		txtTelefone.setBounds(73, 87, 367, 20);
-		add(txtTelefone);
+		txtTelefone.setBounds(72, 104, 352, 20);
+		contentPane.add(txtTelefone);
+		
+		txtTipo = new JTextField();
+		txtTipo.setColumns(10);
+		txtTipo.setBounds(72, 135, 352, 20);
+		contentPane.add(txtTipo);
+		
+		txtHorario = new JTextField();
+		txtHorario.setColumns(10);
+		txtHorario.setBounds(72, 166, 352, 20);
+		contentPane.add(txtHorario);
+		
+		txtPreco = new JTextField();
+		txtPreco.setColumns(10);
+		txtPreco.setBounds(72, 197, 352, 20);
+		contentPane.add(txtPreco);
 		
 		JButton btnNewButton = new JButton("Voltar");
-		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnNewButton.setBounds(10, 266, 89, 23);
-		add(btnNewButton);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				TelaFuncionario telaFunc = new TelaFuncionario(funcioanarioLogado);
+				telaFunc.setVisible(true);
+				dispose();
+			}
+		});
+		btnNewButton.setBounds(10, 227, 89, 23);
+		contentPane.add(btnNewButton);
 		
 		JButton btnCadastrar = new JButton("Cadastrar");
 		btnCadastrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
 				String nome = txtNome.getText();
 				String email = txtEmail.getText();
 				String telefone = txtTelefone.getText();
 				String cpf = txtCPF.getText();
 				String tipo = txtTipo.getText();
-				LocalDateTime horario = LocalDateTime.parse(txtHorario.getText());
+				Date horario = null;
+				try {
+					horario = formatter.parse(txtHorario.getText());
+				} catch (ParseException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 				double preco = Double.parseDouble(txtPreco.getText());
 				
 				Cliente cliente = new Cliente(nome, email, telefone, cpf, 0);
 				
 				Exame exame = new Exame(tipo, horario, preco, cliente);
-				
-				if (cadastroExame.checaHorario(exame)) {
+				if (nome == null || email == null || telefone == null || cpf == null || tipo == null || txtHorario.getText() == null ||  txtPreco.getText()== null) {
+					JOptionPane.showMessageDialog(contentPane,"Erro, campos imcompletos");
+				} else if (Main.EXAMES.checaHorario(exame)) {
 					JOptionPane.showMessageDialog(contentPane,"Horário não disponível");
 				} else {
-					cadastroExame.cadastraExame(exame);
+					Main.EXAMES.cadastraExame(exame);
+					JOptionPane.showMessageDialog(contentPane,"Exame cadastrado");
 				}
-				
-				
 			}
 		});
-		btnCadastrar.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnCadastrar.setBounds(325, 268, 115, 23);
-		add(btnCadastrar);
-		
-		JTextPane txtpnCpf = new JTextPane();
-		txtpnCpf.setText("CPF:");
-		txtpnCpf.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		txtpnCpf.setBackground(SystemColor.menu);
-		txtpnCpf.setBounds(10, 118, 62, 20);
-		add(txtpnCpf);
-		
-		txtCPF = new JTextField();
-		txtCPF.setColumns(10);
-		txtCPF.setBounds(73, 118, 367, 20);
-		add(txtCPF);
-		
-		JTextPane txtpnTipo = new JTextPane();
-		txtpnTipo.setText("Tipo:");
-		txtpnTipo.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		txtpnTipo.setBackground(SystemColor.menu);
-		txtpnTipo.setBounds(10, 149, 62, 20);
-		add(txtpnTipo);
-		
-		txtTipo = new JTextField();
-		txtTipo.setColumns(10);
-		txtTipo.setBounds(73, 149, 367, 20);
-		add(txtTipo);
-		
-		JTextPane txtpnCpf_1_1 = new JTextPane();
-		txtpnCpf_1_1.setText("Horário:");
-		txtpnCpf_1_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		txtpnCpf_1_1.setBackground(SystemColor.menu);
-		txtpnCpf_1_1.setBounds(10, 180, 62, 20);
-		add(txtpnCpf_1_1);
-		
-		txtHorario = new JTextField();
-		txtHorario.setColumns(10);
-		txtHorario.setBounds(73, 180, 367, 20);
-		add(txtHorario);
-		
-		JTextPane txtpnCpf_1_1_1 = new JTextPane();
-		txtpnCpf_1_1_1.setText("Preço:");
-		txtpnCpf_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		txtpnCpf_1_1_1.setBackground(SystemColor.menu);
-		txtpnCpf_1_1_1.setBounds(10, 211, 62, 20);
-		add(txtpnCpf_1_1_1);
-		
-		txtPreco = new JTextField();
-		txtPreco.setColumns(10);
-		txtPreco.setBounds(73, 211, 367, 20);
-		add(txtPreco);
-		
+		btnCadastrar.setBounds(335, 228, 89, 23);
+		contentPane.add(btnCadastrar);
 	}
+
 }
